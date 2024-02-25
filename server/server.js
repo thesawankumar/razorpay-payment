26a47459471e148164c0f590cc998749d6b1dc09
sawan1k <sawankushwaha222@gmail.com>
2024-02-25 16:17:03 +0530

    dist

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import Razorpay from "razorpay";
 import { connect } from "./config/db.js";
 dotenv.config();
+const PORT = process.env.PORT || 6000;
 connect();
 export const instance = new Razorpay({
   key_id: process.env.RAZORPAY_API_KEY,
@@ -12,3 +13,8 @@ export const instance = new Razorpay({
 app.listen(process.env.PORT, () =>
   console.log(`Server is working on ${process.env.PORT}`)
 );
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
