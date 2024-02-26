98443850eda4fe1d022d901e797dbcec1df0b1da
sawan1k <sawankushwaha222@gmail.com>
2024-02-26 19:41:51 +0530

    client dist file error

--- server/index.js
+++ server/index.js
@@ -5,15 +5,18 @@ import Razorpay from "razorpay";
 import { connect } from "./config/db.js";
 import path from "path";
 dotenv.config({ path: "server/.env" });
+
 const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 6000;
+
 connect();
 export const instance = new Razorpay({
   key_id: process.env.RAZORPAY_API_KEY,
   key_secret: process.env.RAZORPAY_APT_SECRET,
 });
 
-app.listen(process.env.PORT, () =>
+app.listen(PORT, () =>
   console.log(`Server is working on ${process.env.PORT}`)
 );
 app.use(express.static(path.join(__dirname, "../client/dist")));
